f325dac6757483dd32dd8d26a33323d98df341bf
Moxxx1e <moxxxie07@gmail.com>
2020-10-27 12:37:02 +0300

    FB-66: Добавить DELETE метод в репозиторий для сессии

--- internal/session/repository.go
+++ internal/session/repository.go
@@ -5,4 +5,5 @@ import "github.com/go-park-mail-ru/2020_2_Slash/internal/models"
 type SessionRepository interface {
 	Insert(session *models.Session) error
 	SelectByValue(sessValue string) (*models.Session, error)
+	DeleteByValue(sessionValue string) error
 }
