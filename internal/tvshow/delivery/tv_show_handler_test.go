ca6552061fa72d1f10989ab0c24c6a21fb97ccbe
OlegGibadulin <O.Gibadulin@yandex.ru>
2020-11-20 10:54:09 +0300

    FB-112: Добавить тесты к методам работы с сериалами

---
+++ internal/tvshow/delivery/tv_show_handler_test.go
@@ -0,0 +1,307 @@
+package delivery
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"strings"
+	"testing"
+
+	actorMocks "github.com/go-park-mail-ru/2020_2_Slash/internal/actor/mocks"
+	contentMocks "github.com/go-park-mail-ru/2020_2_Slash/internal/content/mocks"
+	countryMocks "github.com/go-park-mail-ru/2020_2_Slash/internal/country/mocks"
+	directorMocks "github.com/go-park-mail-ru/2020_2_Slash/internal/director/mocks"
+	genreMocks "github.com/go-park-mail-ru/2020_2_Slash/internal/genre/mocks"
+	"github.com/go-park-mail-ru/2020_2_Slash/internal/models"
+	tvshowMocks "github.com/go-park-mail-ru/2020_2_Slash/internal/tvshow/mocks"
+	"github.com/go-park-mail-ru/2020_2_Slash/pkg/converter"
+	"github.com/go-park-mail-ru/2020_2_Slash/tools/response"
+	"github.com/golang/mock/gomock"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+)
+
+var countries = []*models.Country{
+	&models.Country{
+		ID:   1,
+		Name: "США",
+	},
+}
+
+var genres = []*models.Genre{
+	&models.Genre{
+		Name: "Мультфильм",
+	},
+	&models.Genre{
+		Name: "Комедия",
+	},
+}
+
+var actors = []*models.Actor{
+	&models.Actor{
+		Name: "Майк Майерс",
+	},
+	&models.Actor{
+		Name: "Эдди Мёрфи",
+	},
+}
+
+var directors = []*models.Director{
+	&models.Director{
+		Name: "Эндрю Адамсон",
+	},
+	&models.Director{
+		Name: "Вики Дженсон",
+	},
+}
+
+type Request struct {
+	Name             string   `json:"name" validate:"required,lte=128"`
+	OriginalName     string   `json:"original_name" validate:"required,lte=128"`
+	Description      string   `json:"description" validate:"required"`
+	ShortDescription string   `json:"short_description" validate:"required"`
+	Year             int      `json:"year" validate:"required"`
+	CountriesID      []uint64 `json:"countries" validate:"required"`
+	GenresID         []uint64 `json:"genres" validate:"required"`
+	ActorsID         []uint64 `json:"actors" validate:"required"`
+	DirectorsID      []uint64 `json:"directors" validate:"required"`
+}
+
+func TestTVShowHandler_CreateTVShowHandler(t *testing.T) {
+	t.Parallel()
+	// Setup
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	tvshowUseCase := tvshowMocks.NewMockTVShowUsecase(ctrl)
+	contentUseCase := contentMocks.NewMockContentUsecase(ctrl)
+	countryUseCase := countryMocks.NewMockCountryUsecase(ctrl)
+	genreUseCase := genreMocks.NewMockGenreUsecase(ctrl)
+	actorUseCase := actorMocks.NewMockActorUseCase(ctrl)
+	directorUseCase := directorMocks.NewMockDirectorUseCase(ctrl)
+
+	var contentInst *models.Content = &models.Content{
+		Name:             "Шрек",
+		OriginalName:     "Shrek",
+		Description:      "Полная сюрпризов сказка об ужасном болотном огре, который ненароком наводит порядок в Сказочной стране",
+		ShortDescription: "Полная сюрпризов сказка об ужасном болотном огре",
+		Year:             2001,
+		Countries:        countries,
+		Genres:           genres,
+		Actors:           actors,
+		Directors:        directors,
+		Type:             "tvshow",
+	}
+
+	countriesID := []uint64{1}
+	directorsID := []uint64{1, 2}
+	actorsID := []uint64{1, 2}
+	genresID := []uint64{1, 2}
+
+	requestData := &Request{
+		Name:             "Шрек",
+		OriginalName:     "Shrek",
+		Description:      "Полная сюрпризов сказка об ужасном болотном огре, который ненароком наводит порядок в Сказочной стране",
+		ShortDescription: "Полная сюрпризов сказка об ужасном болотном огре",
+		Year:             2001,
+		CountriesID:      countriesID,
+		GenresID:         genresID,
+		ActorsID:         actorsID,
+		DirectorsID:      directorsID,
+	}
+
+	var tvshowInst *models.TVShow = &models.TVShow{
+		Content: *contentInst,
+	}
+
+	tvshowJSON, err := converter.AnyToBytesBuffer(requestData)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/api/v1/tvshows", strings.NewReader(tvshowJSON.String()))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	tvshowHandler := NewTVShowHandler(tvshowUseCase, contentUseCase,
+		countryUseCase, genreUseCase, actorUseCase, directorUseCase)
+	handleFunc := tvshowHandler.CreateTVShowHandler()
+	tvshowHandler.Configure(e, nil)
+
+	countryUseCase.
+		EXPECT().
+		ListByID(countriesID).
+		Return(countries, nil)
+
+	genreUseCase.
+		EXPECT().
+		ListByID(genresID).
+		Return(genres, nil)
+
+	actorUseCase.
+		EXPECT().
+		ListByID(actorsID).
+		Return(actors, nil)
+
+	directorUseCase.
+		EXPECT().
+		ListByID(directorsID).
+		Return(directors, nil)
+
+	tvshowUseCase.
+		EXPECT().
+		Create(tvshowInst).
+		Return(nil)
+
+	response := &response.Response{Body: &response.Body{"tvshow": tvshowInst}}
+
+	// Assertions
+	if assert.NoError(t, handleFunc(c)) {
+		assert.Equal(t, http.StatusCreated, rec.Code)
+
+		expResBody, err := converter.AnyToBytesBuffer(response)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		bytes, _ := ioutil.ReadAll(rec.Body)
+
+		assert.JSONEq(t, expResBody.String(), string(bytes))
+	}
+}
+
+func TestTVShowHandler_DeleteTVShowHandler(t *testing.T) {
+	t.Parallel()
+	// Setup
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	tvshowUseCase := tvshowMocks.NewMockTVShowUsecase(ctrl)
+	contentUseCase := contentMocks.NewMockContentUsecase(ctrl)
+	countryUseCase := countryMocks.NewMockCountryUsecase(ctrl)
+	genreUseCase := genreMocks.NewMockGenreUsecase(ctrl)
+	actorUseCase := actorMocks.NewMockActorUseCase(ctrl)
+	directorUseCase := directorMocks.NewMockDirectorUseCase(ctrl)
+
+	var contentInst *models.Content = &models.Content{
+		Name:             "Шрек",
+		OriginalName:     "Shrek",
+		Description:      "Полная сюрпризов сказка об ужасном болотном огре, который ненароком наводит порядок в Сказочной стране",
+		ShortDescription: "Полная сюрпризов сказка об ужасном болотном огре",
+		Year:             2001,
+		Countries:        countries,
+		Genres:           genres,
+		Actors:           actors,
+		Directors:        directors,
+	}
+
+	var tvshowInst *models.TVShow = &models.TVShow{
+		Content: *contentInst,
+	}
+
+	e := echo.New()
+	strId := strconv.Itoa(int(tvshowInst.ID))
+	req := httptest.NewRequest(http.MethodPost, "/api/v1/tvshows"+strId, strings.NewReader(""))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetParamNames("id")
+	c.SetParamValues(strId)
+
+	tvshowHandler := NewTVShowHandler(tvshowUseCase, contentUseCase,
+		countryUseCase, genreUseCase, actorUseCase, directorUseCase)
+	handleFunc := tvshowHandler.DeleteTVShowHandler()
+	tvshowHandler.Configure(e, nil)
+
+	tvshowUseCase.
+		EXPECT().
+		GetByID(tvshowInst.ID).
+		Return(tvshowInst, nil)
+
+	contentUseCase.
+		EXPECT().
+		DeleteByID(tvshowInst.ContentID).
+		Return(nil)
+
+	response := &response.Response{Message: "success"}
+
+	// Assertions
+	if assert.NoError(t, handleFunc(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+
+		expResBody, err := converter.AnyToBytesBuffer(response)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		bytes, _ := ioutil.ReadAll(rec.Body)
+
+		assert.JSONEq(t, expResBody.String(), string(bytes))
+	}
+}
+
+func TestTVShowHandler_GetTVShowHandler(t *testing.T) {
+	t.Parallel()
+	// Setup
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	tvshowUseCase := tvshowMocks.NewMockTVShowUsecase(ctrl)
+	contentUseCase := contentMocks.NewMockContentUsecase(ctrl)
+	countryUseCase := countryMocks.NewMockCountryUsecase(ctrl)
+	genreUseCase := genreMocks.NewMockGenreUsecase(ctrl)
+	actorUseCase := actorMocks.NewMockActorUseCase(ctrl)
+	directorUseCase := directorMocks.NewMockDirectorUseCase(ctrl)
+
+	var contentInst *models.Content = &models.Content{
+		Name:             "Шрек",
+		OriginalName:     "Shrek",
+		Description:      "Полная сюрпризов сказка об ужасном болотном огре, который ненароком наводит порядок в Сказочной стране",
+		ShortDescription: "Полная сюрпризов сказка об ужасном болотном огре",
+		Year:             2001,
+		Countries:        countries,
+		Genres:           genres,
+		Actors:           actors,
+		Directors:        directors,
+	}
+
+	var tvshowInst *models.TVShow = &models.TVShow{
+		Content: *contentInst,
+	}
+	var userID uint64 = 0
+
+	e := echo.New()
+	strId := strconv.Itoa(int(tvshowInst.ID))
+	req := httptest.NewRequest(http.MethodPost, "/api/v1/tvshows"+strId, strings.NewReader(""))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetParamNames("mid")
+	c.SetParamValues(strId)
+
+	tvshowHandler := NewTVShowHandler(tvshowUseCase, contentUseCase,
+		countryUseCase, genreUseCase, actorUseCase, directorUseCase)
+	handleFunc := tvshowHandler.GetTVShowHandler()
+	tvshowHandler.Configure(e, nil)
+
+	tvshowUseCase.
+		EXPECT().
+		GetFullByID(tvshowInst.ID, userID).
+		Return(tvshowInst, nil)
+
+	response := &response.Response{Body: &response.Body{"tvshow": tvshowInst}}
+
+	// Assertions
+	if assert.NoError(t, handleFunc(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+
+		expResBody, err := converter.AnyToBytesBuffer(response)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		bytes, _ := ioutil.ReadAll(rec.Body)
+
+		assert.JSONEq(t, expResBody.String(), string(bytes))
+	}
+}
