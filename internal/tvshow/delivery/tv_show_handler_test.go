059fede9cba5c61a778c51383b8748a089236b56
OlegGibadulin <O.Gibadulin@yandex.ru>
2020-12-17 04:43:18 +0300

    FB-148: Добавить метод обновления сериала

--- internal/tvshow/delivery/tv_show_handler_test.go
+++ internal/tvshow/delivery/tv_show_handler_test.go
@@ -178,6 +178,122 @@ func TestTVShowHandler_CreateTVShowHandler(t *testing.T) {
 	}
 }
 
+func TestTVShowHandler_UpdateTVShowHandler(t *testing.T) {
+	t.Parallel()
+	// Setup
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	tvshowUseCase := tvshowMocks.NewMockTVShowUsecase(ctrl)
+	contentUseCase := contentMocks.NewMockContentUsecase(ctrl)
+	countryUseCase := countryMocks.NewMockCountryUsecase(ctrl)
+	genreUseCase := genreMocks.NewMockGenreUsecase(ctrl)
+	actorUseCase := actorMocks.NewMockActorUseCase(ctrl)
+	directorUseCase := directorMocks.NewMockDirectorUseCase(ctrl)
+	seasonUseCase := seasonMocks.NewMockSeasonUsecase(ctrl)
+
+	isFree := true
+	var contentInst *models.Content = &models.Content{
+		Name:             "Шрек",
+		OriginalName:     "Shrek",
+		Description:      "Полная сюрпризов сказка об ужасном болотном огре, который ненароком наводит порядок в Сказочной стране",
+		ShortDescription: "Полная сюрпризов сказка об ужасном болотном огре",
+		Year:             2001,
+		IsFree:           &isFree,
+		Countries:        countries,
+		Genres:           genres,
+		Actors:           actors,
+		Directors:        directors,
+	}
+
+	countriesID := []uint64{1}
+	directorsID := []uint64{1, 2}
+	actorsID := []uint64{1, 2}
+	genresID := []uint64{1, 2}
+
+	requestData := &Request{
+		Name:             "Шрек",
+		OriginalName:     "Shrek",
+		Description:      "Полная сюрпризов сказка об ужасном болотном огре, который ненароком наводит порядок в Сказочной стране",
+		ShortDescription: "Полная сюрпризов сказка об ужасном болотном огре",
+		Year:             2001,
+		IsFree:           &isFree,
+		CountriesID:      countriesID,
+		GenresID:         genresID,
+		ActorsID:         actorsID,
+		DirectorsID:      directorsID,
+	}
+
+	var tvshowInst *models.TVShow = &models.TVShow{
+		Content: *contentInst,
+	}
+
+	tvshowJSON, err := converter.AnyToBytesBuffer(requestData)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	e := echo.New()
+	strId := strconv.Itoa(int(tvshowInst.ID))
+	req := httptest.NewRequest(http.MethodPost, "/api/v1/tvshows"+strId, strings.NewReader(tvshowJSON.String()))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetParamNames("tid")
+	c.SetParamValues(strId)
+	c.Set("userID", 3)
+
+	tvshowHandler := NewTVShowHandler(tvshowUseCase, contentUseCase,
+		countryUseCase, genreUseCase, actorUseCase, directorUseCase, seasonUseCase)
+	handleFunc := tvshowHandler.UpdateTVShowHandler()
+	tvshowHandler.Configure(e, nil)
+
+	countryUseCase.
+		EXPECT().
+		ListByID(countriesID).
+		Return(countries, nil)
+
+	genreUseCase.
+		EXPECT().
+		ListByID(genresID).
+		Return(genres, nil)
+
+	actorUseCase.
+		EXPECT().
+		ListByID(actorsID).
+		Return(actors, nil)
+
+	directorUseCase.
+		EXPECT().
+		ListByID(directorsID).
+		Return(directors, nil)
+
+	tvshowUseCase.
+		EXPECT().
+		GetByID(tvshowInst.ID).
+		Return(tvshowInst, nil)
+
+	contentUseCase.
+		EXPECT().
+		UpdateByID(tvshowInst.ContentID, contentInst).
+		Return(contentInst, nil)
+
+	response := &response.Response{Body: &response.Body{"tvshow": tvshowInst}}
+
+	// Assertions
+	if assert.NoError(t, handleFunc(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+
+		expResBody, err := converter.AnyToBytesBuffer(response)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		bytes, _ := ioutil.ReadAll(rec.Body)
+
+		assert.JSONEq(t, expResBody.String(), string(bytes))
+	}
+}
+
 func TestTVShowHandler_DeleteTVShowHandler(t *testing.T) {
 	t.Parallel()
 	// Setup
